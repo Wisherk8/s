c9cf14725480505e1affccb76d20816dea9b4098
Wisherk8 <Wisherk8@gmail.com>
2021-08-27 14:56:59 +0300

    Update as.sh

--- as.sh
+++ as.sh
@@ -1,11 +1,11 @@
 #wget https://nl.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-virt-3.14.2-x86_64.iso
 #dd
 mkdir /media/setup
-cp -a /media/sda/* /media/setup
+cp -a /media/sda1/* /media/setup
 mkdir /lib/setup
 cp -a /.modloop/* /lib/setup
 /etc/init.d/modloop stop
-umount /dev/sda
-mv /media/setup/* /media/sda/
+umount /dev/sda1
+mv /media/setup/* /media/sda1/
 mv /lib/setup/* /.modloop/
 setup-alpine
